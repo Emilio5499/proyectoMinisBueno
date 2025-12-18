7f137599a5f902491c2ff4a353aed92722095d6a
Emilio5499 <1448592@alu.murciaeduca.es>
2025-12-18 12:49:23 +0100

    currando mas

--- database/migrations/0001_01_01_000001_create_users_table.php
+++ database/migrations/0001_01_01_000001_create_users_table.php
@@ -17,11 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->foreignId('role_id')->constrained()->cascadeOnDelete();
             $table->rememberToken();
             $table->timestamps();
         });
-
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
